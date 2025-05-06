74635fc948f5e0361220c3ae74cb3d725919d8dc
Diego Perez <Diego.fernando.perez.luna@gmail.com>
2025-05-06 17:37:27 -0400

    Add: CORS as a middleware to app.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from src.routes import search_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Ontology Search API", version="1.0.0")
 
 app.include_router(search_router.router, prefix="/api", tags=["search"])
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
